f0ff7f0e45c1b35d518eb07b16ee96d8fc4a9c42
MillerDB <millerberrio1116427@correo.itm.edu.co>
2025-11-25 23:01:28 -0500

    Mas Servlets

--- Frontend/Frontend/src/main/java/com/example/fronted/Configs/ServletConfig.java
+++ Frontend/Frontend/src/main/java/com/example/fronted/Configs/ServletConfig.java
@@ -1,7 +1,6 @@
 package com.example.fronted.Configs;
 
-import com.example.fronted.Servlets.LoginServlet;
-import com.example.fronted.Servlets.RegistroServlet;
+import com.example.fronted.Servlets.*;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +10,55 @@ public class ServletConfig {
 
     @Bean
     public ServletRegistrationBean<RegistroServlet> registroServlet() {
-        return new ServletRegistrationBean<>(new RegistroServlet(), "/RegistroServlet");
+        ServletRegistrationBean<RegistroServlet> bean = new ServletRegistrationBean<>(
+                new RegistroServlet(), "/RegistroServlet"
+        );
+        bean.setLoadOnStartup(1);
+        return bean;
     }
 
     @Bean
     public ServletRegistrationBean<LoginServlet> loginServlet() {
-        return new ServletRegistrationBean<>(new LoginServlet(), "/LoginServlet");
+        ServletRegistrationBean<LoginServlet> bean = new ServletRegistrationBean<>(
+                new LoginServlet(), "/LoginServlet"
+        );
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
+
+    @Bean
+    public ServletRegistrationBean<DashboardServlet> dashboardServlet() {
+        ServletRegistrationBean<DashboardServlet> bean = new ServletRegistrationBean<>(
+                new DashboardServlet(), "/DashboardServlet"
+        );
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
+
+    @Bean
+    public ServletRegistrationBean<CrearBilleteraServlet> crearBilleteraServlet() {
+        ServletRegistrationBean<CrearBilleteraServlet> bean = new ServletRegistrationBean<>(
+                new CrearBilleteraServlet(), "/CrearBilleteraServlet"
+        );
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
+
+    @Bean
+    public ServletRegistrationBean<MostrarBilleterasServlet> mostrarBilleterasServlet() {
+        ServletRegistrationBean<MostrarBilleterasServlet> bean = new ServletRegistrationBean<>(
+                new MostrarBilleterasServlet(), "/MostrarBilleterasServlet"
+        );
+        bean.setLoadOnStartup(1);
+        return bean;
+    }
+
+    @Bean
+    public ServletRegistrationBean<LogoutServlet> logoutServlet() {
+        ServletRegistrationBean<LogoutServlet> bean = new ServletRegistrationBean<>(
+                new LogoutServlet(), "/LogoutServlet"
+        );
+        bean.setLoadOnStartup(1);
+        return bean;
     }
 }
